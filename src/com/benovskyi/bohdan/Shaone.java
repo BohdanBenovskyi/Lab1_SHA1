686884f1d4558e7c2efd27c83a480c6d0e4d359c
BohdanBenovskyi <benovskiy1994@gmail.com>
2017-10-05 21:37:57 +0300

    split digest to 16 32-bits words

--- src/com/benovskyi/bohdan/Shaone.java
+++ src/com/benovskyi/bohdan/Shaone.java
@@ -7,18 +7,22 @@ public class Shaone {
 	public static void main(String[] args) {
 		String msg = "Benovskyi";
 		
+		String lenOfMsg = String.format("%64s", Integer.toBinaryString(msg.length()*8)).replace(' ', '0');
+		System.out.println("Бінарне представлення розміру повідомлення: " + lenOfMsg);
+		
 		byte[] inBits = msg.getBytes();
 		System.out.println("Масив байтів: " + Arrays.toString(inBits));
 		System.out.println("Довжина повідомлення (символів): " + inBits.length);
 		
 		String binStr = "";
-		for(int i = 0; i<inBits.length; i++) {
-			//binStr += Integer.toBinaryString(inBits[i]);
+		for(int i = 0; i<inBits.length; i++)
 			binStr += String.format("%8s", Integer.toBinaryString(inBits[i])).replace(' ', '0');
-		}
+		
 		System.out.println("Повідомлення в бітах: " + binStr);
 		System.out.println("Довжина повідомлення (бітів): " + binStr.length());
 		
+		binStr += "1";
+		System.out.println("Одиничний біт був доданий!");
 		
 		if(binStr.length() < 448)
 			while(binStr.length() < 448)
@@ -26,6 +30,28 @@ public class Shaone {
 		
 		System.out.println("Повідомлення в бітах (після додавання нулів): " + binStr);
 		System.out.println("Довжина повідомлення (бітів, після додавання нулів): " + binStr.length());
+		
+		binStr += lenOfMsg;
+		System.out.println("Повідомлення в бітах (після додавання довжини вхідного повідомлення): " + binStr);
+		System.out.println("Довжина повідомлення (бітів, після додавання нулів): " + binStr.length());
+		
+		String tmpBin = binStr;
+		String[] msgBlock = new String [80];
+		
+		//розбив на 16 32-х бітних слова
+		int j = 0;
+		while(!tmpBin.isEmpty()) {
+			String subTmp = "";
+			for(int i = 0; i<32; i++) {
+				subTmp += tmpBin.charAt(i);
+			}
+			
+			for(int i = 0; i<32; i++)
+				tmpBin = new StringBuilder(tmpBin).deleteCharAt(0).toString();
+			
+			msgBlock[j] = subTmp;
+			j++;
+		}
 		
 		
 	}
